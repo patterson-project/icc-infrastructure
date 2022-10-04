2a2b673693112d3b511ab02881b853684b2a7e58
canadrian72 <patterson.n.adrian@gmail.com>
2022-10-03 22:55:48 -0400

    Formatting

--- Cli/cli_installer.sh
+++ Cli/cli_installer.sh
@@ -2,8 +2,8 @@
 
 printf '%s\n' 'Installing dependencies...'
 sudo apt-get update > /dev/null
-sudo apt-get -y install python3-pip > /dev/null
-pip3 install -r requirements.txt --quiet
+sudo apt-get -y install python3-pip 
+pip3 install -r requirements.txt 
 
 cp ../Cli/cli.py icc
 sudo chmod +x icc
@@ -13,4 +13,5 @@ sudo rm icc
 icc_infrastructure_dir=$(dirname $PWD)
 sudo grep -q "^export ICC_INFRASTRUCTURE_PATH=" ~/.bashrc && sudo sed "s|^export ICC_INFRASTRUCTURE_PATH=.*|ICC_INFRASTRUCTURE_PATH=$icc_infrastructure_dir|" -i ~/.bashrc || sudo sed "$ a\export ICC_INFRASTRUCTURE_PATH=$icc_infrastructure_dir" -i ~/.bashrc
 
-printf '%s\n' 'Done! Run source ~/.bashrc or logout & login for these changes to take effect. \nRun "icc --help" for usage.'
+printf '%s\n' 'Done! Run source ~/.bashrc or logout & login for these changes to take effect.'
+printf '%s\n' 'Run "icc --help" for usage.'
